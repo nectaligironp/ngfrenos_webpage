184aa59418cdd66db4a2c97e4db1308342b99e6d
nectaligironp <nectaligironp@gmail.com>
2021-05-29 01:02:56 +0800

    secondlogin

---
+++ README.md
@@ -0,0 +1,4 @@
+# NGFrenos Page
+
+This page is for NGFRENOS
+Images are located in the image folder
---
+++ dynamics.js
@@ -0,0 +1,19 @@
+let peaklogoindex=document.getElementById('peakautologopng');
+peaklogoindex.onmouseover=function(){
+    peaklogoindex.style.width='300px';
+}
+
+peaklogoindex.onmouseout=function(){
+    peaklogoindex.style.width='250px';
+}
+
+let lucasoilindex=document.getElementById('lucasoillogopng');
+lucasoilindex.onmouseover=function(){
+    lucasoilindex.style.width='300px';
+}
+
+lucasoilindex.onmouseout=function(){
+    lucasoilindex.style.width='250px';
+}
+
+
